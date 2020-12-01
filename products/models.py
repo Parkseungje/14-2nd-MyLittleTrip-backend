f3a1780e682c17700a998a3919783dc5f464bff8
parkseungje <tmdwprpdla@naver.com>
2020-12-01 16:03:42 +0900

    models.py 수정

--- products/models.py
+++ products/models.py
@@ -5,19 +5,20 @@ class Product(models.Model):
     price          = models.IntegerField()
     seat_type_id   = models.ForeignKey('SeatType', on_delete = models.CASCADE)
     remaining_seat = models.IntegerField()
-    tatal_seat     = models.IntegerField()
+    tatal_seats    = models.IntegerField()
 
     class Meta:
         db_table = 'products'
 
 class Airplane(models.Model):
-    airline        = models.ForeignKey('Airline', on_delete = models.CASCADE)
+    airline_id        = models.ForeignKey('Airline', on_delete = models.CASCADE)
     from_region_id = models.ForeignKey('Region', on_delete  = models.CASCADE, related_name='from_region_id')
     to_region_id   = models.ForeignKey('Region', on_delete  = models.CASCADE, related_name='to_region_id')
     from_time      = models.TimeField()
     to_time        = models.TimeField()
     from_date      = models.DateField()
     to_date        = models.DateField()
+    airplane_numbers = models.CharField(max_length=15)
 
     class Meta:
         db_table = 'airplanes'
@@ -35,18 +36,9 @@ class Airline(models.Model):
     class Meta:
         db_table = 'airlines'
 
-class Reservation_age(models.Model):
-    reservation_id = models.ForeignKey('reservations.Reservation', on_delete = models.CASCADE)
-    age_id         = models.ForeignKey('reservations.Age', on_delete         = models.CASCADE)
-    count          = models.IntegerField()
-
-    class Meta:
-        db_table = 'reservation_ages'
-
 class Product_reservation(models.Model):
     reservation_id = models.ForeignKey('reservations.Reservation', on_delete = models.CASCADE)
     product_id     = models.ForeignKey('Product', on_delete                  = models.CASCADE)
-    total_price    = models.IntegerField()
 
     class Meta:
         db_table = 'product_reservations'
--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     name          = models.CharField(max_length = 10)
-    email_address = models.CharField(max_length = 30)
+    email         = models.EmailField(max_length = 30)
     password      = models.CharField(max_length = 30)
     phone_number  = models.CharField(max_length = 20)
     card_number   = models.CharField(max_length = 30, null = True)
