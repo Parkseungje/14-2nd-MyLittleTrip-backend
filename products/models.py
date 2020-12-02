c023ff0cc94d2a135d9c782467fbc619d51a6047
GyuSeok <gyskjng@gmail.com>
2020-12-02 19:03:22 +0900

    add : models.py 추가

--- products/models.py
+++ products/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    airplane       = models.ForeignKey('Airplane', on_delete=models.CASCADE)
+    price          = models.DecimalField(max_digits=15, decimal_places=2)
+    seat_type      = models.ForeignKey('SeatType', on_delete=models.CASCADE)
+    remaining_seat = models.IntegerField()
+    total_seats    = models.IntegerField()
+
+    class Meta:
+        db_table = 'products'
+
+class Airplane(models.Model):
+    airline           = models.ForeignKey('Airline', on_delete=models.CASCADE)
+    from_region       = models.ForeignKey('Region', on_delete=models.CASCADE, related_name='departure_region')
+    to_region         = models.ForeignKey('Region', on_delete=models.CASCADE, related_name='arrival_region')
+    from_date         = models.DateTimeField()
+    to_date           = models.DateTimeField()
+    airplane_numbers  = models.CharField(max_length=15)
+
+    class Meta:
+        db_table = 'airplanes'
+
+class SeatType(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'seat_types'
+
+class Airline(models.Model):
+    name      = models.CharField(max_length=30)
+    image_url = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'airlines'
+
+class Region(models.Model):
+    name         = models.CharField(max_length=20)
+    region_code  = models.CharField(max_length=10)
+    image_url    = models.CharField(max_length=100, null=True)
+
+    class Meta:
+        db_table = 'regions'
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Reservation(models.Model):
+    user        = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    head_count  = models.IntegerField()
+    product     = models.ManyToManyField('products.Product')
+
+    class Meta:
+        db_table = 'reservations'
+
