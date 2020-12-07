2c52aabe49ee05a899c89cd65be0f8457bfa8a91
GyuSeok <gyskjng@gmail.com>
2020-12-08 02:45:23 +0900

    decorator 추가

--- reservations/views.py
+++ reservations/views.py
@@ -7,7 +7,11 @@ from rest_framework.views import APIView
 from .models import Reservation
 from serializers import ReservationSerializer
 
+from user.utils import Login_decorator
+
 class ReservationListView(APIView):
+
+    @Login_decorator
     def post(self, request):
         data = json.loads(request.body)
 
@@ -18,8 +22,22 @@ class ReservationListView(APIView):
         else:
             return JsonResponse(serializer.errors, status=400)
 
+    @Login_decorator
     def get(self, request):
         reservation = Reservation.objects.all()
         serializer = ReservationSerializer(reservation, many=True)
         return JsonResponse({'data': serializer.data}, status=200)
 
+class Reservation(APIView):
+
+    @Login_decorator
+    def get(self, request, pk):
+        pass
+
+    @Login_decorator
+    def patch(self, request, pk):
+        pass
+
+    @Login_decorator
+    def delete(self, request, pk):
+        reservation = Reservation.objects.get(id=pk)
---
+++ users.utils.py
@@ -0,0 +1,22 @@
+import jwt, json
+from django.http           import JsonResponse
+
+from .models               import User
+from my_settings import SECRET_KEY,JWT_ALGORITHM
+
+def Login_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            token        = request.headers.get('Authorization', None)
+            payload      = jwt.decode(token, SECRET_KEY, algorithm = JWT_ALGORITHM)
+            user         = User.objects.get(id=payload['id'])
+            request.user = user
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse ({'message':'INVALID_TOKEN'}, status=400)
+
+        except User.DoesNotExist:
+            return JsonResponse ({'message':'INVALID_USER'}, status=400)
+
+        return func(self, request, *args, **kwargs)
+    return wrapper
