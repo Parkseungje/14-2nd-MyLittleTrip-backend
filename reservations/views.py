e7fd010cc1c13320a710adb8e6d0a0e391af9346
GyuSeok <gyskjng@gmail.com>
2020-12-08 03:19:48 +0900

    reservation crud 추가

--- reservations/views.py
+++ reservations/views.py
@@ -7,14 +7,13 @@ from rest_framework.views import APIView
 from .models import Reservation
 from serializers import ReservationSerializer
 
-from user.utils import Login_decorator
+from users.utils import Login_decorator
 
 class ReservationListView(APIView):
-
     @Login_decorator
     def post(self, request):
         data = json.loads(request.body)
-
+        data['user_id'] = request.user.id 
         serializer = ReservationSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
@@ -24,20 +23,28 @@ class ReservationListView(APIView):
 
     @Login_decorator
     def get(self, request):
-        reservation = Reservation.objects.all()
+        reservation = Reservation.objects.get(user=request.user)
         serializer = ReservationSerializer(reservation, many=True)
         return JsonResponse({'data': serializer.data}, status=200)
 
 class Reservation(APIView):
-
     @Login_decorator
+    def get_reservation(self, pk):
+        try:
+            return JsonResponse(Reservation.objects.get(id=pk))
+        except Reservation.DoesNotExist:
+            return JsonResponse({'message': 'Reservation Dose Not Exist'}, status=400)
+
     def get(self, request, pk):
-        pass
+        serializer = ReservationSerializer(get_reservation(pk))
+        return JsonResponse({'data': serializer.data}, status=200)
 
-    @Login_decorator
     def patch(self, request, pk):
-        pass
+        data = json.loads(request.body)
+        serializer = ReservationSerializer(get_reservation(pk), data=data, partial=True)
+        return JsonResponse({'data': serializer.data}, status=200)
 
-    @Login_decorator
     def delete(self, request, pk):
         reservation = Reservation.objects.get(id=pk)
+        reservation.delete()
+        return JsonResponse(status=204)
